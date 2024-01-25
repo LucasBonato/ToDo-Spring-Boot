b5534a3689b8121d0ec787a6d26c9ab7d299ef34
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-01-24 22:11:44 -0300

    Model of User, and added more deppendences (Lombok and Validation)

---
+++ src/main/java/com/lucasbonato/todosimple/models/User.java
@@ -0,0 +1,37 @@
+package com.lucasbonato.todosimple.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    public interface CreateUser {}
+    public interface UpdateUser {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    @NotBlank(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(length = 60, nullable = false)
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    private String password;
+
+    //private List<Task> tasks = new ArrayList<Task>();
+}
